20cf86a0357eb1264a4b8a0a9c06ca31960f4bed
Bhawani <bhawani2322658@gmail.com>
2025-06-23 14:33:21 +0530

    done

---
+++ python19.py
@@ -0,0 +1,68 @@
+#Question 1
+num=int(input("Enter the number of items: "))
+dict={}
+for i in range(num):
+    key=input("Enter the key: ")
+    value=input("Enter the value: ")
+    dict[key]=value
+    print("Dictionary: ",dict)
+
+    print("----------------------------")
+
+ #Question 2 
+    employee_details={101:{'name':"Riya","dept":"Pharmacy","salary":70000},
+    102:{'name':"Navjot","dept","Accounts","salary":80000},
+    103:{'name':"Raman","dept","Law","salary",:80000},
+    104:{'name':"Sumit","dept","Accounts","salary",:450000},
+    105:{'name':"Rohit","dept","Accounts","salary",:80000},
+     list1[]
+    for i in employee_details={i}['salary']>50000:
+list1.append(employee_details[i]['name'])
+print("Employee with salary greater than 50000: "list1)
+
+#Question 
+
+print=float(input("Enter the price of the product: "))
+if price>=1000:,
+print("Prince: ",price)
+print("Discount applied=10%")
+dis=price-(price/20)
+print("Discounted price: "dis)
+elif price<1000 and price>=500:
+print("Price: ",price)
+print("Discount applied= 5%")
+      dis=price-(price/20)
+      print("Discount Price: ",dis)
+      else:
+      print("Price: ",price)
+      print("No discount")
+
+      print("----------------")
+
+      #Question 
+      pwd=input("Enter the password: (at least 8 characters long \nContain both upercase and lowercase characters \ncontains at least one digit \ncontainsat least one special character): ")
+      if len(pwd)>=8:
+      for i in pwd:
+      if i.isupper():
+      up=True
+      elif i.islower():
+      lo=True
+      elif i.isdigit():=+"[]{};:
+        sp=True
+        if up and lo and di and sp:
+        print("Password is strong")
+        else:
+        print("Password is not strong")
+        else:
+        print("Length is less than 8.")
+        print("Password is weak")
+
+        print("------------------------")
+
+        #Question 
+        l1[20,30,40,[57,66,30[70,80],"Hello"]50,True] 
+        li[3][3].insert(1,88)
+        print("List after inserting 88: ",li)
+        li[3].insert(1,88)
+        print("List after inersting 88: ",li)
+        print("List H: "[3][4][0])
